b5ea2a37ae6302c83ffe35dc87e0612b97c37846
burn-Thor <eothorburn@gmail.com>
2022-07-26 22:43:10 +0100

    sidebar done

---
+++ src/components/Playlists.jsx
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { reducerCases } from '../utils/Constants';
+import { useStateProvider } from '../utils/StateProvider'
+import styled from 'styled-components'; 
+
+
+export default function Playlists() {
+  const [{token, playlists}, dispatch] = useStateProvider();
+  useEffect(()=> {
+    const getPlaylistData = async () => {
+      const response = await axios.get("https://api.spotify.com/v1/me/playlists", {
+      headers: {
+        Authorization:"Bearer " + token,
+        "Content-Type": "application/json",
+      }})
+      const {items} = response.data;
+      const playlists = items.map(({name, id})=> {return {name, id};
+    });
+    dispatch({type: reducerCases.SET_PLAYLISTS, playlists})
+    };
+    getPlaylistData();
+  }, [token, dispatch])
+  return (
+    <Container>
+      <ul>
+        {playlists.map(({name, id})=> {
+          return(
+            <li key={id}>{name}</li>
+          )
+        })}
+      </ul>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+      height: 100%;
+      overflow: hidden;
+          ul{
+            list-style: none;
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+            padding: 1rem;
+            height: 50vh;
+            max-height: 100%;
+            overflow: auto;
+            &::-webkit-scrollbar{
+              width: 0.7rem;
+              &-thumb{
+                background-color:gray;
+              }
+            }
+            li{
+                display: flex;
+                gap: 1rem;
+                cursor: pointer;
+                transition: 0.3s ease-in-out;
+                &:hover{
+                    color: white;
+                }
+            }
+        }
+`
